07a0e4cf665418f50ca9526d264e79cd5f2c6d08
Railko <dimon14026@gmail.com>
2025-10-12 17:07:53 +0300

    solved 3

---
+++ task1/task1.3.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+    val firstString = "omg i love shrek"
+    val secondString = "o kek"
+
+    var uniqueSecondStrings: MutableSet<Char> = mutableSetOf()
+    var firstStringList: MutableList<String> = mutableListOf()
+
+    for (i in secondString) {
+        if (i.toString() != " ") {
+            uniqueSecondStrings.add(i)
+            println(i)
+        }
+    }
+
+    for (i in firstString) {
+        firstStringList.add(i.toString())
+    }
+
+    for (char in uniqueSecondStrings) {
+        for (i in 0..firstString.count() - 1) {
+            if (firstStringList[i] == char.toString()) {
+                firstStringList[i] = char.toString() + char.toString()
+            }
+        }
+    }
+    println(firstString)
+    println(uniqueSecondStrings.joinToString(""))
+    println(firstStringList.joinToString(""))
+
+
+}
