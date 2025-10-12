c2ff7abe4b1b13bd8ebe07d17d5b05282a5ba8e2
Railko <dimon14026@gmail.com>
2025-10-12 17:07:46 +0300

    solved 2

---
+++ task1/task1.2.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    val string = " slovo, skdsdsks sdsaowq! Cogito, Ergo Sum."
+    val specialCharactersList = listOf(" ", "!", "?", ",", ".", "(", ")")
+
+    var wordLength: Double = 0.0
+    var lengthList = mutableListOf<Double>()
+
+    for (i in string) {
+        if (i.toString() !in specialCharactersList) {
+            wordLength += 1
+            } else {
+                if (wordLength !== 0.0) {
+                    lengthList.add(wordLength)
+                }
+                wordLength = 0.0
+            }
+    }
+
+    val result: Double = lengthList.sum() / lengthList.count()
+    println(lengthList)
+    println(result)
+
+}
