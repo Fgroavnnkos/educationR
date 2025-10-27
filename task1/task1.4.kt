a839ced8c0ad959a96cf02d9e94beb2efb501154
Fgroavnnkos <dimon14026@gmail.com>
2025-10-27 18:44:55 +0300

    deleted .jar after optimized 4

--- task1/task1.4.kt
+++ task1/task1.4.kt
@@ -3,7 +3,7 @@ fun main() {
     var num1 = "51232162625165382153876521783518253183528198289098210"
     var num2 = "74763984790384091048387528732498714812138217378273917"
     val result: MutableList<String> = mutableListOf()
-    var afterSum = 0
+    var sum, afterSum
     var num3 = ""
 
     val gamma = listOf<String>("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
@@ -19,8 +19,10 @@ fun main() {
     }
 
     for (i in num1.count() - 1 downTo 0) {
-        result.add( ( ( num1[i].toString().toInt() + num2[i].toString().toInt() + afterSum.toInt() ) % 10 ).toString())
-        afterSum =  (num1[i].toString().toInt() + num2[i].toString().toInt() + afterSum.toInt()) / 10
+
+        sum =  num1[i].digitToInt() + num2[i].digitToInt() + afterSum
+        result.add( ( sum % 10 ).toString())
+        afterSum =  sum / 10
     }
 
     if (afterSum != 0) {
