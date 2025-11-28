0202f06ed3f83d2a632d2b2d49d6cfe39807a220
Fgroavnnkos <dimon14026@gmail.com>
2025-11-28 12:11:35 +0300

    i am still doing something

--- task2/task2.1.kt
+++ task2/task2.1.kt
@@ -1,12 +1,55 @@
-fun main() {
-    
-class Student(val email: String) {
+package task2_1
 
-    val fullName: String = email.split("@")[0]
+import org.jetbrains.annotations.Nullable
 
+class Student() {
+ init {
+
+ }
+    var _mail = ""
+    var _name = ""
+    var _surname = ""
+
+    constructor(
+        mail: String,
+        name: String,
+        surname: String,
+    ): this()
+    {
+         if (name.isNotBlank() and surname.isNotBlank())
+        {
+            _name = name
+            _surname = surname
+            val fullName =  "$_name.$_surname" // Можно было записать как _name + '.' + _surname, но мне показалось, что использованный вариант лучше читается
+            _mail = "$fullName@gmail.com"
+            return
+        } else if(mail.isNotBlank() and ("@" in mail) and (mail.split("@")[0].length >= 2))
+        {
+            _mail = mail
+            val fullName = mail.split("@")[0]
+
+
+        } else
+
+    }
 }
+    fun equals(a: Student): Boolean {
 
-val student1 = Student("123321@mail.lol")
-println(student1.fullName)
+        if (mail == a.mail)
+        {
+            return true
+        } else return false
+
+    }
+
+    fun hashCode(a: Student, b: Student): Boolean {
+
+    }
+}
+fun main() {
+
+    val student = Student("ivan", "gorodTver")
+
+    println(student.mail)
 
 }
